a71707f9f5b2a6ea7c3d471fbcd3ef48fbf34674
Rogerio Moura <rogerio.s.moura@hotmail.com>
2020-01-01 21:19:40 -0300

    adicionando listar e paginação

--- view/paginasRestritas/Home.php
+++ view/paginasRestritas/Home.php
@@ -39,8 +39,119 @@
         </nav>
         <!--principal-->
         <main class="col-10">
-            <a href="/crud_php/view/FormPessoa.php?form=2">Cadastrar uma Pessoa</a>
-            <div style="height: 500px; background-color: aqua;">asd</div>
+            <a class="btn btn-outline-dark btn-sm" href="/crud_php/view/FormPessoa.php?form=2">Cadastrar uma Pessoa</a>
+            <!--listar pessoas cadastradas-->
+            <table class="table table-hover table-dark table-sm">
+                <thead>
+                <tr>
+                    <th scope="col">Tipo de Usuario</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Apelido</th>
+                    <th scope="col">Login</th>
+                    <th scope="col">RG</th>
+                    <th scope="col">CPF</th>
+                    <th scope="col">Numero</th>
+                    <th scope="col">Residencial</th>
+                    <th scope="col">E-mail</th>
+                    <th scope="col">Disciplina</th>
+                </tr>
+                </thead>
+                <tbody>
+
+                <tr>
+                    <?php
+                    require_once $_SERVER['DOCUMENT_ROOT'] . "/crud_php/DAO/PessoaDAO.php";
+                    $pessoaDAO = new PessoaDAO();
+
+                    //busca todas as tuplas do banco de dados
+                    $listar = $pessoaDAO->listarTodasPessoas();
+
+                    //pegando o numero total de tuplas no banco de dados para coloca como contador de paginas
+                    $totalTuplas = count($listar);
+
+                    //teste
+                    //pegar paginação atual
+                    $paginaAtual = (empty($_GET['pagina'])) ? "1" : $_GET['pagina'];
+                    //numero de tuplas mostrado por pagina
+                    $itensPorPagina = 10;
+                    //limitador de guias da paginação + guia atual
+                    $limitePaginação = 2;
+                    //inicio da paginação
+                    $inicioParametroTupla = ($itensPorPagina * $paginaAtual) - $itensPorPagina;
+
+                    //definindo o numero total de abas da paginação
+                    $totalpaginacao = ceil($totalTuplas / $itensPorPagina);
+
+                    //sql para puchar o itens da pagina dentro do limite
+                    $buscaPessoasLimit = $pessoaDAO->listarPessoasLimit($inicioParametroTupla, $itensPorPagina);
+
+
+                    foreach ($buscaPessoasLimit as $c) {
+                        echo "<tr>";
+                        echo "  <td>" . $c["id_pessoa"] . "</td>";
+                        echo "  <td>" . $c["descricao"] . "</td>";
+                        echo "  <td>" . $c["nome"] . "</td>";
+                        echo "  <td>" . $c["apelido"] . "</td>";
+                        echo "  <td>" . $c["login"] . "</td>";
+                        echo "  <td>" . $c["rg"] . "</td>";
+                        echo "  <td>" . $c["cpf"] . "</td>";
+                        echo "  <td>" . $c["numero"] . "</td>";
+                        echo "  <td>" . $c["residencial"] . "</td>";
+                        echo "  <td>" . $c["email"] . "</td>";
+                        echo "  <td>" . $c["disciplina"] . "</td>";
+                        echo "</tr>";
+                    }
+                    ?>
+                </tr>
+                </tbody>
+            </table>
+
+            <!--            paginação-->
+
+            <nav aria-label="Navegação de página exemplo">
+                <ul class="pagination">
+
+                    <?php
+                    //so mostra o botao inicio se nao estiver na pagina 1
+                    if ($paginaAtual > 1) {
+                        //guia primeiro
+                        echo '<li class="page-item"><a class="page-link" href="Home.php?pagina=1">Primeira</a></li>';
+                        //guia setinhas volta
+                        echo '<a class="page-link" href="Home.php?pagina='.($paginaAtual - 1).'" aria-label="Anterior">
+                                <span aria-hidden="true">&laquo;</span>
+                                <span class="sr-only">Anterior</span>
+                              </a>';
+                    }
+
+                    //$j = ($paginaAtual < 2)?1:;
+
+                    for ($i = ($paginaAtual-$limitePaginação); $i <= ($paginaAtual+$limitePaginação); $i++) {
+                        if($i>=1 && $i <= $totalpaginacao) {
+                            $ativo = ($i == $paginaAtual) ? "page-item active" : "page-item";
+                            echo '<li class="' . $ativo . '">';
+                            echo '    <a class="page-link" href="Home.php?pagina=' . $i . ' ">' . $i . '</a>';
+                            echo ' </li>';
+                        }
+                    }
+
+                    //so mostra o botao ultimo se nao estiver na ultima pagina
+                    if ($paginaAtual < $totalpaginacao) {
+                        //guia setinhas avança
+                        echo '<a class="page-link" href="Home.php?pagina='.($paginaAtual + 1).'" aria-label="Próximo">
+                                <span aria-hidden="true">&raquo;</span>
+                                <span class="sr-only">Próximo</span>
+                              </a>';
+                        echo '<li class="page-item">
+                                <a class="page-link" href="Home.php?pagina=' . $totalpaginacao . '">Última</a>
+                              </li>';
+                    }
+                    ?>
+
+
+                </ul>
+            </nav>
+
+
         </main>
     </div>
 
